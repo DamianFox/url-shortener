92adba133fe8ff970a2bf635c1d308bbea245710
damiano.fossa <d.fossa.91@gmail.com>
2018-04-26 23:27:43 +0200

    server script

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// Define the port to run on
+app.set('port', 3000);
+
+// Add middleware to console log every request
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next(); 
+});
+
+// Set static directory before defining routes
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+// Enable parsing of posted forms
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// var routes = require('./routes');
+
+// Add some routing
+// app.use('/', routes);
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});

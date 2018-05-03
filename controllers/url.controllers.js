eaf8cadcbdd407809688d316db40815da12dfe0b
damiano.fossa <d.fossa.91@gmail.com>
2018-05-03 22:59:47 +0200

    GET one specific Url by ID

--- controllers/url.controllers.js
+++ controllers/url.controllers.js
@@ -40,6 +40,38 @@ module.exports.urlAddOne = function(req, res) {
 	}
 };
 
+// GET one specific Url by ID
+module.exports.urlGetOne = function(req, res) {
+
+	var id = req.params.urlId;
+
+	console.log('GET urlId', id);
+
+	Url
+	.findById(id)
+	.exec(function(err, doc) {
+	  var response = {
+	    status : 200,
+	    message : doc
+	  };
+	  if (err) {
+	    console.log("Error finding url");
+	    response.status = 500;
+	    response.message = err;
+	  } else if(!doc) {
+	    console.log("urlId not found in database", id);
+	    response.status = 404;
+	    response.message = {
+	      "message" : "url ID not found " + id
+	    };
+	  }
+	  res
+	    .status(response.status)
+	    .json(response.message);
+	});
+};
+
+
 function validateURL(url) {
     // Regex from https://gist.github.com/dperini/729294
     var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,9 @@ router
   .route('/new')
   .post(ctrlUrls.urlAddOne);
 
+router
+  .route('/urls/:urlId')
+  .get(ctrlUrls.urlGetOne)
+
 
 module.exports = router;
